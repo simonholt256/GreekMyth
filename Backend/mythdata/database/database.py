82f0f342f0eb7fca75878eee4254ffb9fae0bd51
simonholt-flyinglizard <simonvholt92@gmail.com>
2026-06-12 22:23:49 +0100

    changing config import string

--- Backend/mythdata/database/database.py
+++ Backend/mythdata/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from config import URL_DATABASE
+from mythdata.config import URL_DATABASE
 
 engine = create_engine(URL_DATABASE, future=True, echo=True)
 
