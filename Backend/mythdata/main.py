f1a9961aa9fec0ae967a0654005c92c271de6cfd
simonholt-flyinglizard <simonvholt92@gmail.com>
2026-06-15 15:31:41 +0100

    open up cors to test its working

--- Backend/mythdata/main.py
+++ Backend/mythdata/main.py
@@ -9,10 +9,7 @@ from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
